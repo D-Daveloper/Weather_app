ac8092743985e8106c5f631d33a5b0a317905eb2
DefinitelynotZombie <davidmuoegbunam@gmail.com>
2024-04-26 21:37:26 +0100

    fixed some bugs and wrote some extra media queries

--- public/style.js
+++ public/style.js
@@ -4,3 +4,42 @@ setInterval(() => {
     let d = new Date();
     time.innerHTML = d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 },1000)
+
+let locationButton = document.querySelector(".location")
+
+locationButton.addEventListener("click",() =>{
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+
+                const data = { latitude, longitude };
+
+                // Send location data to backend server
+                fetch('/api/location', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
+                .then(response => {
+                    if (response.ok) {
+                        console.log("Location data sent successfully to server");   
+                    } else {
+                        console.error("Failed to send location data to server");
+                    }
+                })
+                .catch(error => {
+                    console.error("Error sending location data:", error);
+                });
+            },
+            error => {
+                console.error("Error getting geolocation:", error);
+            }
+        );
+    } else {
+        console.error("Geolocation is not supported by this browser");
+    }
+})
